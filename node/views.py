1f65cae21ab88268ad77f2d47793242436344608
s-ov <s.ovsiuk@gmail.com>
2024-09-14 14:32:59 +0300

    update node

--- node/views.py
+++ node/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+
+import logging
 
 from .models import Node, NodeMotor
 from .forms import (
                     NodeCreationForm, 
                     NodeMotorCreationForm, 
-                    NodeMotorForm, 
-                    NodeForm,
-                    NodeIndexForm,
-                    NodeUpdateForm
                     )
 
 
@@ -91,6 +88,45 @@ def node_detail_view(request, node_id):
     return render(request, 'node/node_detail.html', context)
 
 
+def retrieve_node_detail_view(request):
+    """
+    Handles retrieving a Node instance based on the entered index.
+    
+    Args:
+        request (HttpRequest): The HTTP request object containing metadata about the request.
+
+    Returns:
+        HttpResponse: The rendered template displaying the details of the Node.
+    """
+    
+    if request.method == 'POST':
+        index = request.POST.get('index')
+
+        if not index:
+            return render(
+                request, 
+                'node/node_detail.html', 
+                {'error': 'Введіть, буль ласка, індекс.'}
+                )
+
+        try:
+            node = Node.objects.get(index=index)
+            return render(
+                request, 
+                'node/node_detail.html',
+                  {'node': node}
+                  )
+        
+        except Node.DoesNotExist:
+            return render(
+                request, 
+                'node/node_detail.html', 
+                {'error': f'Вузол з індексом {index} не знайдено.'})
+
+    return render(request, 'node/node_detail.html')
+
+
+
 def pre_change_data_view(request):
     """
     Displays the endpoints to change the data of the NodeMotor and Node instances.
@@ -131,16 +167,12 @@ def update_node_motor_view(request):
             if 'update' in request.POST:
                 motor_id = request.POST.get('motor_id')  
                 motor = NodeMotor.objects.get(id=motor_id)
-                form = NodeMotorForm(request.POST, instance=motor)
+                form = NodeMotorCreationForm(request.POST, instance=motor)
                 
                 if form.is_valid():
                     form.save()  
                     return redirect('node:motors_list')  
-                else:
-                    return render(
-                        request, 
-                        'node/update_motor_form.html', 
-                        {'form': form, 'motor': motor})
+                
             else:
                 motor = NodeMotor.objects.filter(power=power).first()
 
@@ -151,7 +183,7 @@ def update_node_motor_view(request):
                         {'error': f'Не знайдено двигун з потужністю {power}.'}
                         )
 
-                form = NodeMotorForm(instance=motor)
+                form = NodeMotorCreationForm(instance=motor)
                 return render(
                     request, 
                     'node/update_motor_form.html', 
@@ -170,12 +202,52 @@ def update_node_motor_view(request):
 
 def update_node_view(request):
     """
-    Handles the Node instance updating with the provided form data.
-
+    Handles the Node instance update based on the entered index.
+    
     Args:
-        request (HttpRequest): The HTTP request object containing metadata about the request.
-        node_id (int): The ID of the Node instance to be retrieved.
+        request (HttpRequest): The HTTP request object.
 
     Returns:
-        HttpResponse: The rendered template displaying the updated details of the Node.
+        HttpResponse: The rendered template displaying the form to update the Node.
     """
+    
+    if request.method == 'POST':
+        index = request.POST.get('index')
+        
+        if not index:
+            return render(
+                request, 
+                'node/update_node_form.html', 
+                {'error': 'Введіть, буль ласка, індекс.'}
+            )
+
+        try:
+            node = Node.objects.get(index=index)
+
+            if 'update' in request.POST:
+                form = NodeCreationForm(request.POST, instance=node)
+                if form.is_valid():
+                    form.save()
+                    return redirect('node:node_list')  
+            else:
+                form = NodeCreationForm(instance=node)
+
+            return render(
+                request, 
+                'node/update_node_form.html', 
+                {'form': form, 'node': node}
+            )
+        except Node.DoesNotExist:
+            return render(
+                request, 
+                'node/update_node_form.html', 
+                {'error': f'Вузол з індексом {index} не знайдено.'}
+            )
+        except ValueError:
+            return render(
+                request, 
+                'node/update_node_form.html', 
+                {'error': 'Не правильний ввід. Введіть індекс знову.'}
+            )
+        
+    return render(request, 'node/update_node_form.html')
